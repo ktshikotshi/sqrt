0cc79aa9521b784dd3e680e4cf1483e43146cab0
Khuthadzo Tshikotshi <khuthadzotshikotshi@gmail.com>
2017-10-28 18:14:32 +0200

    Add project files.

---
+++ sqrt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sqrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
